245ab1c59c787f2d8b6a26aec0f223e7de9ce0f3
Jose Martinez <arkharl@LunaMBA.local>
2018-01-25 00:43:07 -0600

    role set as API resource, added jwt_extended, working w/ vue

--- resources/role.py
+++ resources/role.py
@@ -3,18 +3,13 @@ from models.role import RoleModel
 
 class Role(Resource):
   parser = reqparse.RequestParser()
-  parser.add_argument('role_name',
-    type = str,
-    required = True,
-    help = 'role needs a name'
-  )
   parser.add_argument('is_admin', type = bool)
 
-  def post(self):
+  def post(self, name):
     data = Role.parser.parse_args()
-    role_name = data['role_name']
+    # role_name = data['role_name']
 
-    if RoleModel.find_by_name(role_name):
+    if RoleModel.find_by_name(name):
       return {'message', 'A role with this name already exists'}, 400
 
     is_admin = data['is_admin']
@@ -22,7 +17,7 @@ class Role(Resource):
     if is_admin is None:
       is_admin = False
 
-    role = RoleModel(role_name, is_admin)
+    role = RoleModel(name, is_admin)
 
     try:
       role.save()
--- server.py
+++ server.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask_restful import Api
-from flask_jwt import JWT
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    get_jwt_identity
+)
 
 from resources.user import UserRegister, User
 from resources.role import Role
@@ -12,32 +15,48 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-app.config['JWT_AUTH_USERNAME_KEY'] = 'email'
+
+# Setup the Flask-JWT-Extended extension
+app.config['JWT_SECRET_KEY'] = 'super-secret2'  # Change this!
+jwt = JWTManager(app)
+
+app.config['CORS_HEADERS'] = 'Content-Type'
 app.secret_key = 'secret'
-#cors = CORS(app, resources={r"*": {"origins": "*"}})
+cors = CORS(app, supports_credentials = True)
 
 api = Api(app)
 
-jwt = JWT(app, authenticate, identity) # /auth
+# jwt = JWT(app, authenticate, identity) # /auth
 
 # move to run.py when deploying to heroku
 @app.before_first_request
 def create_tables():
   db.create_all()
 
-@app.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    if request.method == 'OPTIONS':
-        response.headers['Access-Control-Allow-Methods'] = 'DELETE, GET, POST, PUT'
-        headers = request.headers.get('Access-Control-Request-Headers')
-        if headers:
-            response.headers['Access-Control-Allow-Headers'] = headers
-    return response
-
 @app.route('/')
 def home():
   return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    if not request.is_json:
+        return jsonify({"message": "Missing JSON in request"}), 400
+
+    email = request.json.get('email', None)
+    password = request.json.get('password', None)
+    if not email:
+        return jsonify({"message": "Missing email parameter"}), 400
+    if not password:
+        return jsonify({"message": "Missing password parameter"}), 400
+
+    user = authenticate(email, password)
+
+    if not user:
+        return jsonify({"message": "Bad email or password"}), 401
+
+    # Identity can be any data that is json serializable
+    access_token = create_access_token(identity = email)
+    return jsonify(access_token = access_token, user = user.json()), 200
 
 api.add_resource(UserRegister, '/register')
 api.add_resource(Role, '/role/<string:name>')
